736cabbfeef21de2d60c61abdaccf6bc27d986c8
Rick <hoa98hoa@gmail.com>
2022-05-18 15:55:15 -0300

    Created Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        component={ Login }
+      />
+      <Route
+        path="/carteira"
+        component={ Wallet }
+      />
+      <Route
+        path="*"
+        component={ NotFound }
+      />
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (<div>not found</div>);
+  }
+}
+
+export default NotFound;
