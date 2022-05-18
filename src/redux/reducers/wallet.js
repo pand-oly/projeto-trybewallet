87c641bffe514f212facb139fb3f553a0682b429
Rick <hoa98hoa@gmail.com>
2022-05-18 15:22:35 -0300

    Config redux

---
+++ src/redux/reducers/wallet.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
